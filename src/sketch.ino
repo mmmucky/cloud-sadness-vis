c5747bddc7226a669070f80b0ff978f40051e8cc
mmmucky <jamespd@gmail.com>
2016-08-16 07:24:04 +1200

    clean-up

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,9 +1,19 @@
 #include <Adafruit_NeoPixel.h>                                                                                                                                                                                                                 
-//#ifdef __AVR__
-//  #include <avr/power.h>
-//#endif
+// NeoPixel Data line
 #define PIN 7
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(28, PIN, NEO_GRB + NEO_KHZ800);
+
+// The rainbow comprises four arcs, starting from the bottom, they are
+// zero-indexed as follows...
+#define BLUE   0
+#define RED    1
+#define YELLOW 2
+#define GREEN  3
+
+#define WHITE  255
+#define BLACK  0
+
 
 int row_count = 4;
 int row_offsets[5] = {0,4,9,16,28};
@@ -30,66 +40,58 @@ int falloff_brightness[11] = {255, 50, 25, 10, 0, 0, 0, 0, 0, 0, 0};
 int phy_pixel_map[28] = {0, 1, 2, 3, 8, 7, 6, 5, 4, 9, 10, 11, 12, 13, 14, 15, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16};
 
 
-
 void setup() {
-// This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-//  #if defined (__AVR_ATtiny85__)
-//    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-//  #endif
-// End of trinket special code
   strip.begin();
   strip.show();
   Serial.begin(9600);
 }
 
+// custom non-linear brightness functions.
 int custom_brightness(int func, int brightness) {
   switch(func) {
-      case 1:
-          return brightness;
+    case 1:
+      return brightness;
   }
 }
 
 void loop() {
-
   int i, d, a, b, f;
   d=0;
   strip.setBrightness(32);
+
   while(1) {
-    f=1;
-    for (b=0;b<=255;b++) {
-      for (i=0;i<4;i++) {
-        //arc(i, custom_brightness(f, b));
-        arc(i, b);
-      }
-      strip.show();
-      delay(d);
-    }
-    for (b=255;b>=0;b--) {
-      for (i=0;i<4;i++) {
-        //arc(i, custom_brightness(f, b));
-        arc(i, b);
-      }
-      strip.show();
-      delay(d);
-    }
-    Serial.println("derp");
+    flash_arc(GREEN, 50);
+    delay(50);
   }
-/*  while(1) {
-  for (i=0;i<12;i++) {
-      sector(0,i, 1); delay(d);
-    }
-    for (i=12;i>=0;i--) {
-      sector(0,i, 1); delay(d);
-    }
-  }
-  while(1) {
-  for (i=0;i<=12;i++) {
-      sector(0,i, 1); delay(10);
-    }
-    for (i=0;i<12;i++) {
-      sector(i,12, 1); delay(10);
-    }
-  }*/
+
+//  while(1) {
+//    pulse_rainbow(0, 3);
+//  }
+
+
+// vu style grow right, shrink back to the left
+//  while(1) {
+//    // left -> right sector
+//    for (i=0;i<12;i++) {
+//      sector(0,i, 1); delay(d);
+//    }
+//    // right -> left sector
+//    for (i=12;i>=0;i--) {
+//      sector(0,i, 1); delay(d);
+//    }
+//  }
+
+
+// clock-wise rotation
+//  while(1) {
+//    for (i=0;i<=12;i++) {
+//      sector(0,i, 1); delay(10);
+//    }
+//    delay(400);
+//    for (i=0;i<12;i++) {
+//      sector(i,12, 1); delay(10);
+//    }
+//  }
 
 // initial brightness of each arc
 //  int arc_brightness[4] = {0, 85, 170, 255};
@@ -126,55 +128,47 @@ void loop() {
   }
 }
 
-void arc(int arc, int brightness) {
-  int c;
-  uint32_t color;
-  color = strip.Color(brightness,brightness,brightness);
-  for(c=row_offsets[arc]; c<row_offsets[arc+1]; c++) {
-    strip.setPixelColor(c, color);
-  }
+void flash_arc(int arc_num, int d) {
+  arc(arc_num, 255);
+  strip.show();
+  delay(d);
+  arc(arc_num, 0);
   strip.show();
 }
 
+void pulse_rainbow(int d, int step) {
+  int i, b;
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-  
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+    for (b=0;b<=255;b+=step) {
+      for (i=0;i<4;i++) {
+        arc(i, b);
+      }
+      strip.show();
+      delay(d);
     }
-    strip.show();
-    delay(wait);
-  }
-}   
+    for (b=255;b>=0;b-=step) {
+      for (i=0;i<4;i++) {
+        arc(i, b);
+      }
+      strip.show();
+      delay(d);
+    }
+}
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-  
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }   
-}       
-    
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+// light one of the four arcs with constant brightness
+void arc(int arc, int brightness) {
+  int i;
+  uint32_t color = strip.Color(brightness,brightness,brightness);
+  if (brightness > 255)
+    brightness = 255;
+  if (brightness < 0)
+    brightness = 0;
+
+  // loop over pixels in a particular arc
+  for(i=row_offsets[arc]; i<row_offsets[arc+1]; i++) {
+    strip.setPixelColor(i, color);
   }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  strip.show();
 }
 
 void sector(int left, int right, int fade) {
@@ -210,3 +204,50 @@ void sector(int left, int right, int fade) {
       strip.show();
 delay(20);
 }
+
+
+
+// to be removed...
+
+
+//
+//void rainbow(uint8_t wait) {
+//  uint16_t i, j;
+//  
+//  for(j=0; j<256; j++) {
+//    for(i=0; i<strip.numPixels(); i++) {
+//      strip.setPixelColor(i, Wheel((i+j) & 255));
+//    }
+//    strip.show();
+//    delay(wait);
+//  }
+//}   
+//
+//// Slightly different, this makes the rainbow equally distributed throughout
+//void rainbowCycle(uint8_t wait) {
+//  uint16_t i, j;
+//  
+//  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+//    for(i=0; i< strip.numPixels(); i++) {
+//      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+//    }
+//    strip.show();
+//    delay(wait);
+//  }   
+//}       
+//    
+//// Input a value 0 to 255 to get a color value.
+//// The colours are a transition r - g - b - back to r.
+//uint32_t Wheel(byte WheelPos) {
+//  WheelPos = 255 - WheelPos;
+//  if(WheelPos < 85) {
+//    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//  }
+//  if(WheelPos < 170) {
+//    WheelPos -= 85;
+//    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//  }
+//  WheelPos -= 170;
+//  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//}
+
